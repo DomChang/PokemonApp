3cf6d8ea4a257a5c7bb0746fda625bb72d4fc4a8
Dominic Chang <bull216312@gmail.com>
2022-09-29 21:39:22 +0800

    Create PokemonData Model

---
+++ PokemonApp/PokemonApp/Model/PokemonData.swift
@@ -0,0 +1,52 @@
+//
+//  Pokemon.swift
+//  PokemonApp
+//
+//  Created by ChunKai Chang on 2022/9/29.
+//
+
+import Foundation
+
+// MARK: - PokemonList
+struct PokemonData: Codable {
+    let count: Int
+    let next: String
+    let previous: String
+    let results: [PokemonResult]
+}
+
+// MARK: - Result
+struct PokemonResult: Codable {
+    let name: String
+    let url: String
+}
+
+// MARK: - Pokemon
+struct Pokemon: Codable {
+    let id: Int
+    let name: String
+    let sprites: Sprites
+    let height: Int
+    let weight: Int
+    let types: [Attribute]
+}
+
+// MARK: - Sprites
+struct Sprites: Codable {
+    let frontDefault: String
+    
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+    }
+}
+
+// MARK: - TypeElement
+struct TypeElement: Codable {
+    let type: Attribute
+}
+
+// MARK: - Species
+struct Attribute: Codable {
+    let name: String
+    let url: String
+}
