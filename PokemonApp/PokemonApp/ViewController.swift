e49bf59964fc11a59844b3c3ffaef46f452e229d
Dominic Chang <bull216312@gmail.com>
2022-09-29 20:56:11 +0800

    Create initial project

---
+++ PokemonApp/PokemonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonApp
+//
+//  Created by ChunKai Chang on 2022/9/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
